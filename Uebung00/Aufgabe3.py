087ebe9028b8fd2e92a462a6b2c64d4a83e28d1d
ceron21 <mbp@chris-ge.de>
2020-10-12 23:22:23 +0200

    Fixed the issue that a number in secret could be found multiple times - found positions are now blocked There is still and edge case when numbers occure multiple times in secret - the counter of half right numbers could be decreased altough it would be right if a number was guesses at the right and wrong spot

--- Uebung00/Aufgabe3.py
+++ Uebung00/Aufgabe3.py
@@ -2,6 +2,7 @@ secret = "1537"
 anzahl = 0
 anzahlf = 0
 raten = ""
+found = "" #will contain the positions of found numbers, to make sure that one number isn't found twice
 while not raten == secret:
   raten = input('Errate die vierstellige Ziffer: ')
 
@@ -10,9 +11,19 @@ while not raten == secret:
     if secret[i] == raten[i]:
       #print( 'Die ' + str(i+1) +'. Ziffer ist korrekt!')
       anzahl = anzahl + 1
-    elif not -1 == secret.find(raten[i]):
+
+      #decreating half correct counter in the case that a number was "found" at the wrong spot
+      #but later found in the right spot -> right spot is more important that false spot
+      if str(i) in found: 
+        anzahlf -= 1
+
+      found += str(i) #strorring number at this position was found
+
+                                              #checking that this position wasn't found earlier
+    elif not -1 == secret.find(raten[i]) and str(secret.find(raten[i])) not in found:
       #print ('Die ' + str(raten[i]) +'. Ziffer ist in den Secret, aber an der falschen Stelle!')
       anzahlf = anzahlf + 1
+      found += str(secret.find(raten[i])) #strorring number at this position was found
 
   if not anzahl == 0:     
     print(str(anzahl) + " Ziffern sind richtig.") 
@@ -25,5 +36,6 @@ while not raten == secret:
 
   anzahlf = 0
   anzahl = 0
+  found = ""
  
 print("Du hast es erratten!")
